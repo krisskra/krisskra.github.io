16c943fa6bad9d319043f4a16c3c7dbfcc9ad5f9
Krzysztof Iskra <krzysztof.iskra@onet.pl>
2020-11-19 18:43:35 +0100

    Zadanie 26744 odwroc string do sprawdzenia

---
+++ zadania-domowe/Zadanie26744/script.js
@@ -0,0 +1,10 @@
+let txt = (text) =>{
+        let tab = [...text];
+        let tab2 = tab.reverse();
+        tab3 = tab2.join('');
+        console.log(tab3);
+        return tab3
+}
+txt('Akademia108')
+
+
